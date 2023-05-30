af79a2a4955411b2b5493db2e740f84e3c46b1ca
iusmar.lng@gmail.com <iusmar.lng@gmail.com>
2023-05-30 12:03:48 +0200

    Adding Basic Security

---
+++ TodoApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace TodoApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    private IConfiguration _configuration;
+
+    public AuthenticationController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public record AuthenticationData(string? Username, string? Password);
+    public record UserData(int Id, string? FirstName, string? LastName, string? UserName);
+
+    [HttpPost("token")]
+    [AllowAnonymous]
+    public ActionResult<string> Authenticate([FromBody] AuthenticationData data)
+    {
+        var user = ValidateCredentials(data);
+
+        if (user == null)
+        {
+            return Unauthorized();
+        }
+
+        var token = GenerateToken(user);
+        
+        return Ok(token);
+    }
+
+    private string GenerateToken(UserData user)
+    {
+        var securityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_configuration.GetValue<string>("Authentication:SecretKey")));
+        var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+
+        List<Claim> claims = new();
+            claims.Add(new(JwtRegisteredClaimNames.Sub, user.Id.ToString()));
+            claims.Add(new(JwtRegisteredClaimNames.UniqueName, user.UserName));
+            claims.Add(new(JwtRegisteredClaimNames.FamilyName, user.LastName));
+            claims.Add(new(JwtRegisteredClaimNames.GivenName, user.FirstName));
+
+        var token = new JwtSecurityToken(
+            _configuration.GetValue<string>("Authentication:Issuer"),
+            _configuration.GetValue<string>("Authentication:Audience"),
+            claims,
+            expires: DateTime.Now.AddMinutes(30),
+            signingCredentials: signingCredentials);
+            
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+
+    private UserData? ValidateCredentials(AuthenticationData data)
+    {
+        // THIS IS NOT PRODUCTION CODE - REPLACE WITH YOUR OWN AUTHENTICATION LOGIC
+
+        if (CompareValues(data.Username, "mlungu") && CompareValues(data.Password, "test123"))
+        {
+            return new UserData(1, "Marius", "Lungu", data.Username!);
+        }
+
+        if (CompareValues(data.Username, "admin") && CompareValues(data.Password, "admin"))
+        {
+            return new UserData(2, "Admin", "Adminescu", data.Username!);
+        }
+
+        return null;
+    }
+
+    private bool CompareValues(string? actual, string? expected)
+    {
+        if (actual is not null)
+        {
+            if (actual.Equals(expected)) 
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,15 +1,34 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddAuthorization(_ =>
+    _.FallbackPolicy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build());
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer(_ =>
+    {
+        _.TokenValidationParameters = new()
+        {
+            ValidateAudience = true,
+            ValidateIssuer = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration.GetValue<string>("Authentication:Issuer"),
+            ValidAudience = builder.Configuration.GetValue<string>("Authentication:Audience"),
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(
+                builder.Configuration.GetValue<string>("Authentication:SecretKey")))
+        };
+    });
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +36,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
