f383d70dd0d0263cd4f67f546c0e766b2716d385
iusmar.lng@gmail.com <iusmar.lng@gmail.com>
2023-05-30 14:22:28 +0200

    create the controllers

---
+++ ToDoLibrary/Models/TodoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoLibrary.Models;
+
+public class TodoModel
+{
+    public int Id { get; set; }
+    public string? TaskValue { get; set; }
+    public int AssignedTo { get; set; }
+    public bool IsComplete { get; set; }
+}
---
+++ TodoApi/Controllers/ToDosController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoLibrary.Models;
+
+namespace TodoApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ToDosController : ControllerBase
+{
+    // GET: api/Todos
+    [HttpGet]
+    public ActionResult<IEnumerable<TodoModel>> Get()
+    {
+        throw new NotImplementedException();
+    }
+
+    // GET api/Todos/5
+    [HttpGet("{id}")]
+    public ActionResult<TodoModel> Get(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    // POST api/Todos
+    [HttpPost]
+    public ActionResult<TodoModel> Post([FromBody] string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    // PUT api/Todos/5
+    [HttpPut("{id}")]
+    public IActionResult Put(int id, [FromBody] string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut("{id}/Complete")]
+    public IActionResult Complete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    // DELETE api/Todos/5
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+    
+}
